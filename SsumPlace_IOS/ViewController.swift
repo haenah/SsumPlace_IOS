78b3fcaffaa73ed9a264e5c45592afd4bd73e170
jaewon <paramecium6@gmail.com>
2020-02-17 20:37:48 +0900

    Initial Commit

---
+++ SsumPlace_IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SsumPlace_IOS
+//
+//  Created by 안재원 paramecium6 on 2020/02/17.
+//  Copyright © 2020 안재원 paramecium6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
